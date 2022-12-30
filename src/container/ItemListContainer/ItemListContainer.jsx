77c821f8e103018c5ef07d1b64bd1182e3e08e21
noelia-andrada <110588488+noelia-andrada@users.noreply.github.com>
2022-12-30 15:17:01 -0300

    Error firebase

--- src/container/ItemListContainer/ItemListContainer.jsx
+++ src/container/ItemListContainer/ItemListContainer.jsx
@@ -1,26 +1,32 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ItemList from "../../components/ItemList/ItemList";
-import gFetch from "../../components/ItemProductList/ItemProductList";
 import './ItemListContainer.css'
+import {doc, getDocs, getFirestore, limit, orderBy, query, where} from "firebase/firestore"
 
 function ItemListContainer ({greeting}) {
     const greetingStyle = {fontSize: "200%"}
 
     const [product , setProd] = useState({})
     const [loading, setLoading] = useState (true)
+    const [dbProduct, setDbProd] = useState({})
 
     const {categoriaId} = useParams()
 
     useEffect(()=> {
+        const db = getFirestore()
+        const queryCollection = doc(db,"productos")
+
         if(categoriaId){
-            gFetch()
-            .then (data => setProd(data.filter(prod=>prod.categoria === categoriaId)))
+            const queryFiltrada = query(queryCollection, where("categoria", "==", categoriaId))
+    
+            getDocs(queryFiltrada)
+            .then(resp => setDbProd(resp.docs.map(producto => ({categoriaId: producto.categoriaId, ...producto.resp()}) )))
             .catch(err => err)
             .finally(()=>setLoading(false))
-        }else{
-            gFetch()
-            .then (data => setProd(data))
+        }else{    
+            getDocs(queryCollection)
+            .then(resp => setDbProd(resp.docs.map(producto => ({categoriaId: producto.categoriaId, ...producto.resp()}) )))
             .catch(err => err)
             .finally(()=>setLoading(false))
         }
